e590e1993b04fb00326aafc13fb1af7154727c57
Rajan <rajansaini691@gmail.com>
2020-03-09 17:21:37 -0700

    Updates

--- model.py
+++ model.py
@@ -1,11 +1,33 @@
-from keras.layers import Conv2D, Dense, Activation
+from keras.layers import Conv2D, Dense, Dropout, Activation, MaxPooling2D
 from keras.models import Sequential
+import pickle
 
+# Create model
 model = Sequential([
-    Conv2D(32, 5, 1, padding="same", data_format="channels_last"),
+    Conv2D(32, kernel_size=5, strides=1, padding="same",
+           data_format="channels_last"),
     Activation('relu'),
-    Dense(32, input_shape=(784,)),
+    MaxPooling2D(strides=(2, 2)),
+    Conv2D(64, kernel_size=5, strides=1),
     Activation('relu'),
+    MaxPooling2D(strides=(2, 2)),
+    Dense(1024),
+    Dropout(0.4),
     Dense(10),
     Activation('softmax')
 ])
+
+model.compile(optimizer="rmsprop",
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+# Load data
+with open('./cifar-10-batches-py/data_batch_1', 'rb') as d1:
+    data = pickle.load(d1, encoding='bytes')
+
+# Train model
+model.fit(data, epochs=2, validation_split=0.8)
+
+# Evaluate model
+
+# Print results
